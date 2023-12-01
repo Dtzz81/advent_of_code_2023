b495ee728f59491e7269f8b9910192fbc7acfb57
dtzz81 <denisa.turkova@madetech.com>
2023-12-01 15:43:32 +0000

    first test added

---
+++ day_1/src/finding_numbers.py
@@ -0,0 +1,7 @@
+
+class CalibrationReader:
+    def read(self, calibration_line):
+        #self.calibration_line = calibration_line
+        #assign an object member/value called self.calibration_line on the left with the value of
+        #the parameter called calibration_line on the right
+        return 12
